74847f7a4bc710d61556c9ca9d75ebd6de356b67
adrian <adrian.kmh@gmx.de>
2025-01-30 14:55:25 +0100

    endlich! es ist gut

--- laden.py
+++ laden.py
@@ -14,12 +14,13 @@ def train():
         with open("Daten/data_batch_"+str(i+1),"rb") as pi:
             data = pickle.load(pi, encoding='latin1') 
         X += data["data"].tolist()
-        y += data["labels"]      
+        y += data["labels"]  
     
     #Daten werden für das Modell angepasst(zu Numpy Array mit richtigen Dimensionen und normalisiert)
-    X = np.array(X)
+    X = np.array(X, dtype=np.float32)
     X = X / 255.0
-    X = X.reshape((50000, 32, 32, 3))
+    X = X.reshape((50000, 3, 32, 32))
+    X = X.transpose(0, 2, 3, 1)
     
     # Output-Daten werden zu Array umgewandelt, dass Warscheinlichkeiten zeigt
     y = pd.get_dummies(data=y,columns=dict,dtype=int).to_numpy()
@@ -33,8 +34,9 @@ def test():
         data = pickle.load(pi, encoding='latin1') 
     
     #Test-Daten werden angepasst
-    X = data["data"] / 255
-    X = X.reshape((10000, 32, 32, 3))
+    X = data["data"] / 255.0
+    X = X.reshape((10000, 3, 32, 32))
+    X = X.transpose(0, 2, 3, 1)
     y = pd.get_dummies(data=data["labels"],columns=dict,dtype=int).to_numpy()
     
     return [X,y]
--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import pickle
-import pandas as pd
+import laden
+import numpy as np
+import matplotlib.pyplot as plt
+import show
 
-with open("Daten/batches.meta","rb") as pi:
-    dict = pickle.load(pi, encoding='latin1')["label_names"]
 
-with open("Daten/test_batch","rb") as pi:
-    data = pickle.load(pi, encoding='latin1') 
+bilder = laden.train()[0]
+bild = bilder[0]
 
-y = pd.get_dummies(data=data["labels"],columns=dict,dtype=int).to_numpy()
 
-print(y[0:10])
 
-print(data["labels"][:10])
+plt.imshow(bild)
+plt.show()
+
+show.image(1,0)
